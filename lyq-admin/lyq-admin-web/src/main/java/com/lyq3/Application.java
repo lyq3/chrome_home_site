fed04d17d883bff220cb5806a67417c795943a43
罗云强 <99190092qq.com>
2017-08-06 15:58:31 +0800

    测通Dubbo

---
+++ lyq-admin/lyq-admin-web/src/main/java/com/lyq3/Application.java
@@ -0,0 +1,13 @@
+package com.lyq3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:dubbo-consum.xml")
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
