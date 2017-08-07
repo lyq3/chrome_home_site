d3988d73d323ee67934cb441efd981a06e0100eb
罗云强 <99190092qq.com>
2017-08-07 21:06:22 +0800

    加上Swagger注解

--- lyq-admin/lyq-admin-web/src/main/java/com/lyq3/Application.java
+++ lyq-admin/lyq-admin-web/src/main/java/com/lyq3/Application.java
@@ -3,9 +3,11 @@ package com.lyq3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ImportResource("classpath:dubbo-consum.xml")
+@EnableSwagger2
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
