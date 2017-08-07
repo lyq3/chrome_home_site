e8d1f6c1b82447d8c05c9bd6f352922667c15289
罗云强 <99190092@qq.com>
2017-08-07 21:53:49 +0800

    1

--- lyq-admin/lyq-admin-web/src/main/java/com/lyq3/controller/UserController.java
+++ lyq-admin/lyq-admin-web/src/main/java/com/lyq3/controller/UserController.java
@@ -5,7 +5,6 @@ import com.lyq3.api.UserService;
 import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
