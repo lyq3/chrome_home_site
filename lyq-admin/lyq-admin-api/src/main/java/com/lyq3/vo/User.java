1b3bdce0c74c75b9734902be8467fbee8f80acfe
罗云强 <99190092@qq.com>
2017-08-12 11:03:32 +0800

    增加角色实体类

--- lyq-admin/lyq-admin-api/src/main/java/com/lyq3/vo/User.java
+++ lyq-admin/lyq-admin-api/src/main/java/com/lyq3/vo/User.java
@@ -1,6 +1,7 @@
 package com.lyq3.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 用户实体类
@@ -8,16 +9,64 @@ import java.io.Serializable;
  * 日期：2017/8/6
  */
 public class User implements Serializable{
+    /**用户ID*/
+    private Integer id;
     /**用户名*/
-    private String username = "";
+    private String userName = "";
     /**密码*/
     private String password = "";
+    /**昵称*/
+    private String nickName = "";
+    /**注册时间*/
+    private Date joinDate;
+    /**最后登录时间*/
+    private Date lastLogin;
 
-    public String getUsername() {
-        return username;
+    public Integer getId() {
+        return id;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
     }
 }
